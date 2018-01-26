631f95611a6307349e24cd1223d9c1a877d25316
nancy sin <nsin@sbcglobal.net>
2018-01-26 20:40:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,19 +2,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0// Write your JavaScript code inside the init() function
-  document.body.addEventListener('keydown', function(e) {
-   const key = parseInt(e.detail || e.which);
-
+  document.body.addEventListener('keydown', function(e){
+   const key = parseInt(e.detail || e.which)
    if (key === alphabet[index]) {
-     index++;
-
+     index++
      if (index === alphabet.length) {
-       alert("Hurray!");
-
-       index = 0;
+       alert("Hurray!")
+       index = 0
      }
    } else {
-     index = 0;
+     index = 0
    }
  })
 }
