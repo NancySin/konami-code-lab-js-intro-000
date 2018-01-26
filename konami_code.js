1d089350af1b62534bc3a45e50e24cdf59a5cc73
nancy sin <nsin@sbcglobal.net>
2018-01-26 20:17:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -9,7 +9,7 @@ const alphabet = [65, 66, 67];
   let index = 0;
 
   // This is the function that would be invoked by the event listener.
-  document.body.addEventListener('keydown', function onKeyDownHandler(e) {
+   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
 
     if (key === alphabet[index]) {
